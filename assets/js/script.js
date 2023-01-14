abe26d27f37f4e03cf4a93c1bc8077a9250a1d35
Abhilash Madi <madiabhilash1563@gmail.com>
2023-01-15 03:43:42 +0530

    navbar completed

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+/*
+ ** #PRELOADING
+ */
+
+const loadElement = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", function () {
+  loadElement.classList.add("loaded");
+});
+
+/*
+** #NAVBAR MOBILE TOGGLE
+*/
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function (){
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+}
+
+navToggler.addEventListener("click",toggleNavbar);
+
+/*
+** #HEADER ON ACTIVE
+*/
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY >= 100){
+        header.classList.add("active");
+    } else {
+        header.classList.remove("acive");
+    }
+});
